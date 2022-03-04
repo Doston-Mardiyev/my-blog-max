756d08fddbb7ff94ad84934b6fd4987631bfd5ce
Doston Mardiyev <dmardiyev96@gmail.com>
2022-03-04 11:58:53 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello  Heruko
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         Hello Heruko
-        </a>
+        
       </header>
     </div>
   );
